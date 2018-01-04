564ab4ae5dc0ea7a676106d0d1d8502edda3cbda
Ilian Sapundshiev <ilian@tagspaces.org>
2018-01-04 10:33:58 +0100

    adding new common functions and checks

--- common.js
+++ common.js
@@ -1,89 +1,118 @@
-/* Copyright (c) 2013-2016 The TagSpaces Authors.
+/* Copyright (c) 2013-present The TagSpaces Authors.
  * Use of this source code is governed by the MIT license which can be found in the LICENSE.txt file. */
 
-"use strict";
-/* globals Mousetrap */
+'use strict';
 
 var isCordova;
 var isWin;
-var isWeb;
+var isWeb = (document.URL.startsWith('http') && !document.URL.startsWith('http://localhost:1212/'));
+
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
+    results = regex.exec(location.search);
+
+  return results === null
+    ? ''
+    : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function getFileContentPromise(fullPath, type) {
+  return new Promise((resolve, reject) => {
+    let fileURL = fullPath;
+
+    // TODO checks if works on cordova and web
+    if (fileURL.indexOf('file://') === -1) {
+      fileURL = 'file://' + fileURL;
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', fileURL, true);
+    xhr.responseType = type || 'arraybuffer';
+    xhr.onerror = reject;
+
+    xhr.onload = () => {
+      const response = xhr.response || xhr.responseText;
+      if (response) {
+        resolve(response);
+      } else {
+        reject('getFileContentPromise error');
+      }
+    };
+    xhr.send();
+  });
+}
 
 $(document).ready(function() {
   isCordova = parent.isCordova;
   isWin = parent.isWin;
   isWeb = parent.isWeb;
 
-// Disable drag events in extensions
+  // Disable drag events in extensions
   $(document).on('drop dragend dragenter dragover', function(event) {
     event.preventDefault();
   });
 
-// Hide all menus in TS on click in extension
-  $(document).on('click' , function(event) {
+  // Hide all menus in TS on click in extension
+  $(document).on('click', function(event) {
     fireHideAllMenusEvent();
   });
 
   function fireHideAllMenusEvent() {
-    var msg = {command: "hideAllMenus"};
-    window.parent.postMessage(JSON.stringify(msg), "*");
+    var msg = { command: 'hideAllMenus' };
+    window.parent.postMessage(JSON.stringify(msg), '*');
   }
 
-// Init about box functionality
+  // Init about box functionality
   $('#aboutExtensionModal').on('show.bs.modal', function() {
     $.ajax({
       url: 'README.md',
       type: 'GET'
-    }).done(function(mdData) {
-      //console.log("DATA: " + mdData);
-      if (marked) {
-        var modalBody = $("#aboutExtensionModal .modal-body");
-        modalBody.html(marked(mdData, {sanitize: true}));
-        handleLinks(modalBody);
-      } else {
-        console.log("markdown to html transformer not found");
-      }
-    }).fail(function(data) {
-      console.warn("Loading file failed " + data);
-    });
+    })
+      .done(function(mdData) {
+        //console.log("DATA: " + mdData);
+        if (marked) {
+          var modalBody = $('#aboutExtensionModal .modal-body');
+          modalBody.html(marked(mdData, { sanitize: true }));
+          handleLinks(modalBody);
+        } else {
+          console.log('markdown to html transformer not found');
+        }
+      })
+      .fail(function(data) {
+        console.warn('Loading file failed ' + data);
+      });
   });
 
   function handleLinks($element) {
-    $element.find("a[href]").each(function() {
-      var currentSrc = $(this).attr("href");
+    $element.find('a[href]').each(function() {
+      var currentSrc = $(this).attr('href');
       $(this).bind('click', function(e) {
         e.preventDefault();
-        var msg = {command: "openLinkExternally", link: currentSrc};
-        window.parent.postMessage(JSON.stringify(msg), "*");
+        var msg = { command: 'openLinkExternally', link: currentSrc };
+        window.parent.postMessage(JSON.stringify(msg), '*');
       });
     });
   }
 
-  $("#aboutButton").on("click", function(e) {
-    $("#aboutExtensionModal").modal({show: true});
+  $('#aboutButton').on('click', function(e) {
+    $('#aboutExtensionModal').modal({ show: true });
   });
 
-  $(".roundButton").on("click", function(e) {
+  $('.roundButton').on('click', function(e) {
     fireHideAllMenusEvent();
   });
 
-// Activating the print functionality
-  $("#printButton").on("click", function(e) {
+  // Activating the print functionality
+  $('#printButton').on('click', function(e) {
     window.print();
   });
 
   if (isCordova) {
-    $("#printButton").hide();
+    $('#printButton').hide();
   }
 
   initSearch();
 });
-
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
-
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
 
 function showSearchPanel(e) {
   //$('#searchToolbar').slideDown(500);
@@ -100,11 +129,11 @@ function cancelSearch() {
 }
 
 function initSearch() {
-  $("#findInFile").on('click', function() {
+  $('#findInFile').on('click', function() {
     showSearchPanel();
   });
 
-  $("#searchExtButton").on('click', function() {
+  $('#searchExtButton').on('click', function() {
     doSearch();
   });
 
@@ -113,12 +142,15 @@ function initSearch() {
   });
 
   $('#searchBox').keyup(function(e) {
-    if (e.keyCode === 13) { // Start the search on ENTER
-      doSearch();}
+    if (e.keyCode === 13) {
+      // Start the search on ENTER
+      doSearch();
+    }
   });
 
   $(window).keyup(function(e) {
-    if (e.keyCode == 27) { // Hide search on ESC
+    if (e.keyCode == 27) {
+      // Hide search on ESC
       cancelSearch();
     }
   });
@@ -127,20 +159,20 @@ function initSearch() {
   //  showSearchPanel();
   //  return false;
   //});
-  window.addEventListener("keyup", function keyup(evt){
-
+  window.addEventListener('keyup', function keyup(evt) {
     var handled = false;
-    var cmd = (evt.ctrlKey ? 1 : 0)  |
-            (evt.altKey ? 2 : 0)   |
-            (evt.shiftKey ? 4 : 0) |
-            (evt.metaKey ? 8 : 0);
+    var cmd =
+      (evt.ctrlKey ? 1 : 0) |
+      (evt.altKey ? 2 : 0) |
+      (evt.shiftKey ? 4 : 0) |
+      (evt.metaKey ? 8 : 0);
     /*
      First, handle the key bindings that are independent whether an input
      control is selected or not.
      */
-    if(cmd === 1 || cmd === 8 || cmd === 5 || cmd === 12){
+    if (cmd === 1 || cmd === 8 || cmd === 5 || cmd === 12) {
       // either CTRL or META key with optional SHIFT.
-      switch(evt.keyCode){
+      switch (evt.keyCode) {
         case 70: // f
           //open custom search/find text
           handled = true;
@@ -149,7 +181,7 @@ function initSearch() {
           //find next
           handled = true;
           break;
-        case 61:  // FF/Mac "="
+        case 61: // FF/Mac "="
         case 107: // FF "+" and "="
         case 187: // Chrome "+"
         case 171: // FF with German keyboard
@@ -166,8 +198,8 @@ function initSearch() {
     }
 
     // CTRL or META without shift
-    if(cmd === 1 || cmd === 8){
-      switch(evt.keyCode){
+    if (cmd === 1 || cmd === 8) {
+      switch (evt.keyCode) {
         case 70: // f
           showSearchPanel(); //open custom search/find text
           handled = true;
@@ -176,8 +208,8 @@ function initSearch() {
     }
 
     // CTRL+ALT or Option+Command
-    if(cmd === 3 || cmd === 10){
-      switch(evt.keyCode){
+    if (cmd === 3 || cmd === 10) {
+      switch (evt.keyCode) {
         case 80: //p
           //presentaion mode
           handled = true;
@@ -188,7 +220,7 @@ function initSearch() {
           break;
       }
     }
-    if(handled){
+    if (handled) {
       evt.preventDefault();
       return;
     }
@@ -198,20 +230,26 @@ function initSearch() {
 function doSearch() {
   //$('#htmlContent').unhighlight();
   $('#searchBox').attr('placeholder', 'Search');
-  var givenString = document.getElementById("searchBox").value;
+  var givenString = document.getElementById('searchBox').value;
 
   var selector = $('#htmlContent') || 'body';
-  var caseSensitiveString = $('#htmlContent').highlight(givenString, {wordsOnly: false});
+  var caseSensitiveString = $('#htmlContent').highlight(givenString, {
+    wordsOnly: false
+  });
   var found, getSelection;
 
-  if (window.find) { // Firefox, Google Chrome, Safari
+  if (window.find) {
+    // Firefox, Google Chrome, Safari
     found = window.find(givenString);
-    $('#htmlContent').highlight(givenString, {wordsOnly: false});
+    $('#htmlContent').highlight(givenString, { wordsOnly: false });
 
-    var searchTermRegEx = new RegExp(found, "ig");
-    var matches = $(selector).text().match(searchTermRegEx);
+    var searchTermRegEx = new RegExp(found, 'ig');
+    var matches = $(selector)
+      .text()
+      .match(searchTermRegEx);
     if (matches) {
-      if ($('.highlight:first').length) {//if match found, scroll to where the first one appears
+      if ($('.highlight:first').length) {
+        //if match found, scroll to where the first one appears
         //$(window).animate({scrollTo:($("*:contains('"+ givenString +"')").offset().top)},"fast");
         //$(selector).animate({scrollTop: $('#htmlContent .highlight::selection').offset().top}, "fast");
         //window.find(givenString);
@@ -219,7 +257,10 @@ function doSearch() {
       }
     }
     if (!found || (!found && !caseSensitiveString) || !caseSensitiveString) {
-      var topOfContent = $(selector).animate({scrollTop: $('#htmlContent').offset().top}, "fast");
+      var topOfContent = $(selector).animate(
+        { scrollTop: $('#htmlContent').offset().top },
+        'fast'
+      );
       //$('#htmlContent').unhighlight();
       $('#searchBox').val('');
       $('#searchBox').attr('placeholder', 'Search text not found. Try again.');
